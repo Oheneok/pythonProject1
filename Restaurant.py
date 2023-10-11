be895789b079c395fb6cb14b7ede8066294c7b57
Oheneok <odurokwame28@gmail.com>
2023-10-11 11:29:28 +0000

    Initial commit

---
+++ Restaurant.py
@@ -0,0 +1,16 @@
+#state parameters
+on = True
+choice = input("What you want chop my guy?")
+print(choice)
+
+#food menu decision
+if choice == "Chicken fillet":
+    print("Price: 65.00")
+elif choice == "Medium rare steak":
+    print("Price: 95.00")
+elif choice == "Spaghetti Bolognese":
+    print("Price: 75.00")
+elif choice == "Fufuo ne ebunuebunu":
+    print("Price: Wo didi dodo masa. 3y3 35 cedis wai!")
+else:
+    print("Masa ebi nni hor o!")
